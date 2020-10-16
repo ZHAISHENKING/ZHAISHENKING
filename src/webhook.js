203f4bb5d2b92c02aef3d1aeab3b515585f24df5
ZHAISHENKING <18700790825@163.com>
2020-10-16 11:38:52 +0800

    创建webhooks

---
+++ src/webhook.js
@@ -0,0 +1,31 @@
+const { Octokit } = require("@octokit/rest");
+const octokit = new Octokit({
+  auth: process.env.GITHUB_SECRET_FOR_NETLIFY,
+});
+
+exports.handler = async function(event, context, callback) {
+
+  if (event.httpMethod !== 'POST') {
+    console.log('Invalid httpMethod:', event.httpMethod);
+
+    callback(null, {
+      statusCode: 200,
+      body: 'Invalid httpMethod',
+    });
+  }
+
+  console.log('>> event received', event);
+  const result = await octokit.actions.createWorkflowDispatch({
+    owner: 'ZHAISHENKING',
+    repo: 'ZHAISHENKING',
+    workflow_id: 'yuque.yml',
+    ref: 'main',
+  });
+
+  console.log('--> handler received', context, result);
+
+  callback(null, {
+    statusCode: 200,
+    body: 'Hello World',
+  });
+}
